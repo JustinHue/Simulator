d934b706c85e4c2ab673fd880c56d3e4cdd0f715
Justin Hellsten <justinhellsten@gmail.com>
2013-06-22 16:53:09 -0400

    Version 0.0.3

--- src/playing_god/Main.groovy
+++ src/playing_god/Main.groovy
@@ -11,40 +11,48 @@ import groovy.swing.j2d.GraphicsPanel
 import java.awt.Dimension
 import java.awt.Frame;
 
+import static javax.swing.JFrame.EXIT_ON_CLOSE
+
+import java.util.logging.FileHandler
+import java.util.logging.Level
 import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter
 
 class Main {
 	
-	static Logger LOGGER = Logger.getLogger("InfoLogging");
+	static Logger LOGGER = Logger.getLogger(Main.class.getName());
+	static FileHandler FH = null
 	
 	static main(args) {
+
+		// DEFINE LOGGER AND FILE HANDLER
+		FH = new FileHandler("Log/main.log", true)
+		FH.setFormatter(new SimpleFormatter())
 		
+		LOGGER.addHandler(FH)
 		
+		LOGGER.setLevel(Level.ALL)
+		LOGGER.log(Level.FINE, "SDFDS")
+		
+		// SET CONFIGURATION FILE AND GRAB CONFIG VALUES
 		Configuration.set_config_file("simulator.cfg")
 		
 		def cfgTitle = Configuration.get_config_value("title", "").toString()
 		def cfgMaximumSize = Configuration.get_config_value("maximumSize", "800,600").toString().split(",")
 		def cfgMinimumSize = Configuration.get_config_value("minimumSize", "800,600").toString().split(",")
 		def cfgPreferredSize = Configuration.get_config_value("preferredSize", "800,600").toString().split(",")
-		
-		def cfgAlignmentX = Configuration.get_config_value("alignmentX", 0).toFloat()
-		def cfgAlignmentY = Configuration.get_config_value("alignmentY", 0).toFloat()
+		def cfgResizable = Configuration.get_config_value("resizable", true).toBoolean()
+		def cfgShowing = Configuration.get_config_value("showing", true).toBoolean()
+		def cfgDefaultCloseOperation = Configuration.get_config_value("defaultCloseOperation", 3).toInteger()
+		def cfgLocationRelativeTo = Configuration.get_config_value("locationRelativeTo", "null") == "null" ? null : null
+		def cfgState = Configuration.get_config_value("state", Frame.NORMAL).toInteger()
+		def cfgUndecorated = Configuration.get_config_value("undecorated", true).toBoolean()
 
+		
+		def cfgLocationX = Configuration.get_config_value("locationX", null)
+		def cfgLocationY = Configuration.get_config_value("locationY", null)
+		
 		/*
-		def reswidth = Configuration.get_config_value("reswidth", 800).toInteger()
-		def resheight = Configuration.get_config_value("resheight", 600).toInteger()
-		def title = Configuration.get_config_value("title", "").toString()
-		def centerWindow = Configuration.get_config_value("centerWindow", true).toBoolean()
-		def windowVisisble = Configuration.get_config_value("windowVisisble", true).toBoolean()
-		def backgroundColor = Configuration.get_config_value("backgroundColor", "black").toString()
-		def innerBorderColor = Configuration.get_config_value("innerBorderColor", "black").toString()
-		//def backgroundWidth = Configuration.get_config_value("backgroundWidth", 800).toInteger()
-		def worldSize = Configuration.get_config_value("worldSize", 32).toInteger()
-		Tile.SIZE = Configuration.get_config_value("tilesize", 32).toInteger()
-		
-		def world = new World(worldSize)
-
-		
 		def gb = new GraphicsBuilder()
 		
 		def go = gb.group {
@@ -65,12 +73,15 @@ class Main {
 			frame = swing.frame(title:cfgTitle, maximumSize:new Dimension(cfgMaximumSize[0].toInteger(), cfgMaximumSize[1].toInteger()), 
 								minimumSize:new Dimension(cfgMinimumSize[0].toInteger(), cfgMinimumSize[1].toInteger()),
 								preferredSize:new Dimension(cfgPreferredSize[0].toInteger(), cfgPreferredSize[1].toInteger()),
-								
-								null, pack:true, show:true) {
+								resizable: cfgResizable, show:cfgShowing, defaultCloseOperation: cfgDefaultCloseOperation,
+								locationRelativeTo: cfgLocationRelativeTo, state:cfgState, undecorated:cfgUndecorated) {
 				
 			}
-							
-
+				
+			if (cfgLocationX != null && cfgLocationY != null) {		
+				frame.setLocation(cfgLocationX.toInteger(), cfgLocationY.toInteger())
+			}
+			
 		}
 
 	}
